2c268395ed371ab3ce491386107dde04262d1c7d
Daisy Johnson <daisyjane.johnson@gmail.com>
2020-04-07 19:58:59 -0600

    javascript

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 function askName() {
   var username = prompt('What is your name?');
-  return document.write('<h3>'+ 'Hello, ' + username + '</h3>');  
+  return document.write('<h3>' + 'Hello, ' + username + '</h3>');
 }
 
 // // Add Greeting
@@ -12,17 +12,17 @@ function addGreeting() {
   var hourNow = today.getHours();
   var greeting;
 
-if (hourNow > 17) {
-  greeting = 'Good evening!';
-} else if (hourNow > 12) {
-  greeting = 'Good afternoon!';
-} else if (hourNow > 0) {
-  greeting = 'Good morning!';
-} else {
-  greeting = 'Welcome!';
-}
+  if (hourNow > 17) {
+    greeting = 'Good evening!';
+  } else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+  } else if (hourNow > 0) {
+    greeting = 'Good morning!';
+  } else {
+    greeting = 'Welcome!';
+  }
 
-return document.write('<h3>'+greeting+'</h3>');  
+  return document.write('<h3>' + greeting + '</h3>');
 }
 
 // // Ask Preference
@@ -30,13 +30,20 @@ return document.write('<h3>'+greeting+'</h3>');
 function askPreference() {
   var response = prompt('Do you prefer cats, dogs or both?')
   var message;
-  if (response === 'cats'){
-      message = 'Cats are purrfect!'
-  } else if (response === 'dogs'){
-      message = 'I love dogs too!'
-  } else if (response === 'both'){
-      message = 'I agree, they both are great!'
-  } else {message = 'Animals are awesome!'}
-  
-  return document.write('<h3>'+ message + '<h3>') 
+  if (response === 'cats') {
+    message = 'Cats are purrfect!'
+  } else if (response === 'dogs') {
+    message = 'I love dogs too!'
+  } else if (response === 'both') {
+    message = 'I agree, they both are great!'
+  } else { message = 'Animals are awesome!' }
+
+  return document.write('<h3>' + message + '<h3>')
 }
+
+function iceCream(iceCreamFlavor, sauce, topping){
+var message = 'I like ' + iceCreamFlavor + ' ice cream with ' + sauce + ' and ' + topping + ' on top!'
+
+return message
+}
+
